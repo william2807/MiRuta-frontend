7a8255ad1308ddf9789bb14827a3a7c393760847
WILLIAM <wilisua4@gmail.com>
2024-04-04 15:22:25 -0500

    agregando dinamismo al formulario, modficacion de estilos y pequeñas modificaciones de la estructura del modulo de inicio de sesion

--- Js/inicioSesion.js
+++ Js/inicioSesion.js
@@ -19,3 +19,33 @@ let boton = document.getElementById("guardar")
 boton.addEventListener("click", ()=>{
     loginUsuario();
 })
+
+const usuario = document.getElementById("usuario");
+const contrasenha = document.getElementById("contrasenha");
+const formulario = document.getElementById("formulario");
+
+let mensajesError = {};
+
+mensajesError.usuarioInvalido = document.getElementById("usuarioInvalido");
+mensajesError.contrasenhaInvalida = document.getElementById("contrasenhaInvalida");
+
+formulario.addEventListener("submit", e=>{
+
+    e.preventDefault();
+    let msjUsuario = "";
+    let msjContranseha = "";
+    ingresar = true;
+
+    if(usuario.value.length < 1){
+        msjUsuario = 'se debe ingresar un usuario valido';
+        ingresar = true;
+    }
+    if(contrasenha.value.length < 1){
+        msjContranseha = 'se debe ingresar una contraseña valida';
+        ingresar = true;
+    }
+    if(ingresar){
+        mensajesError.usuarioInvalido.innerHTML = msjUsuario;
+        mensajesError.contrasenhaInvalida.innerHTML = msjContranseha;
+    }
+});
