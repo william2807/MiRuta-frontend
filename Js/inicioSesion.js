aa27e0f7b06917994b03f6d749e24fa7f2b0000c
WILLIAM <wilisua4@gmail.com>
2024-04-05 17:11:11 -0500

    añadiendo nuevas configuraciones a la interfaz de usuario del modulo de inicio de sesion

--- Js/inicioSesion.js
+++ Js/inicioSesion.js
@@ -36,16 +36,41 @@ formulario.addEventListener("submit", e=>{
     let msjContranseha = "";
     ingresar = true;
 
-    if(usuario.value.length < 1){
-        msjUsuario = 'se debe ingresar un usuario valido';
+    if(usuario.value.trim() < 1 || usuario.value.length < 5){
+        msjUsuario = 'debe ingresar un usuario con al menos 5 caracteres';
         ingresar = true;
     }
-    if(contrasenha.value.length < 1){
-        msjContranseha = 'se debe ingresar una contraseña valida';
+    if(contrasenha.value.trim() < 1 || usuario.value.length < 5){
+        msjContranseha = 'debe ingresar una contraseña con al menos 5 caracteres';
         ingresar = true;
     }
     if(ingresar){
         mensajesError.usuarioInvalido.innerHTML = msjUsuario;
         mensajesError.contrasenhaInvalida.innerHTML = msjContranseha;
     }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    usuario.addEventListener("input", function(){
+        if (usuario.value.trim() === "" || usuario.value.length < 5) {
+            usuario.classList.remove("lleno");
+            usuario.classList.add("vacio");
+        } else {
+            usuario.classList.remove("vacio");
+            usuario.classList.add("lleno");
+        }
+
+    });
+
+    contrasenha.addEventListener("input", function(){
+        if(contrasenha.value.trim() === "" || contrasenha.value.length < 5){
+            contrasenha.classList.remove("lleno");
+            contrasenha.classList.add("vacio");
+        } else{
+            contrasenha.classList.remove("vacio");
+            contrasenha.classList.add("lleno");
+        }
+
+    });
 });
